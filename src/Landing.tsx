17e2d7d2d2bd82886050d1d1ab06aeddb516f7e4
fidas <gabifidalgo2007@gmail.com>
2023-07-12 15:43:02 -0300

    Preparing Landing Page project

---
+++ src/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div className="Landing">
+      Aqui vão estar os componentes da Landing Page!!!
+    </div>
+  );
+}
+
+export default Landing;
